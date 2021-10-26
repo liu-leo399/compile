267242430d45414dad925868b51d30ee3522d47c
liu-leo399 <1804116423@qq.com>
2021-10-26 14:30:01 +0800

    更改

--- Parsing/Parsing.cpp
+++ Parsing/Parsing.cpp
@@ -51,7 +51,7 @@ void Parsing::CompUnit() { //最后写
     }
 
     if ((*nextToken)->getTokenType() == TokenType::INTTK && (*(nextToken + 1))->getTokenType() == TokenType::IDENFR &&
-        (*(nextToken + 2))->getTokenType() == TokenType::LPARENT) {
+        (*(nextToken + 2))->getTokenType() == TokenType::LPARENT || (*nextToken)->getTokenType() == TokenType::VOIDTK) {
         FuncDef();
     }
     MainFuncDef();
@@ -90,13 +90,13 @@ void Parsing::ConstDef() {
         if ((*nextToken)->getTokenType() != TokenType::LBRACK) {
             error();
         }
-        //getNextToken(); //??
+        getNextToken(); // print [
         ConstExp();
         //getNextToken();
         if ((*nextToken)->getTokenType() != TokenType::RBRACK) {
-            getNextToken();
-        } else {
             error();
+        } else {
+            getNextToken();
         }
         //getNextToken();
     }
@@ -112,7 +112,7 @@ void Parsing::ConstInitVal() {
             getNextToken();
             ConstInitVal();
         } while ((*nextToken)->getTokenType() == TokenType::COMMA);
-        getNextToken();
+        //getNextToken();
         if ((*nextToken)->getTokenType() == TokenType::RBRACE)
             getNextToken();
     } else {
@@ -167,15 +167,15 @@ void Parsing::UnaryExp() {
         UnaryExp();
     } else if ((*nextToken)->getTokenType() == TokenType::IDENFR &&
                (*(nextToken + 1))->getTokenType() == TokenType::LPARENT) {
-        getNextToken();
+        getNextToken(); // print iden
         if ((*nextToken)->getTokenType() == TokenType::LPARENT) {
             //error();
-            getNextToken();
+            getNextToken(); // print (
             FuncRParams();
             //getNextToken();
         }
         if ((*nextToken)->getTokenType() != TokenType::RPARENT) error();
-        getNextToken();
+        getNextToken(); // print )
     } else {
         PrimaryExp();
     }
@@ -216,49 +216,57 @@ void Parsing::Number() {
 void Parsing::LVal() {
     // getNextToken();
     if ((*nextToken)->getTokenType() != TokenType::IDENFR)error();
-    //getNextToken();
-    while ((*(nextToken + 1))->getTokenType() == TokenType::LBRACK) {
+    else getNextToken();
+    while ((*(nextToken))->getTokenType() == TokenType::LBRACK) {
         getNextToken();
         Exp();
-        getNextToken();
         if ((*nextToken)->getTokenType() != TokenType::RBRACK) error();
+        else getNextToken();
     }
-    getNextToken();
     fileout << "<LVal>" << std::endl;
 }
 
 void Parsing::FuncRParams() {
     do {
-        getNextToken();
+
         Exp();
-        getNextToken();
-    } while ((*nextToken)->getTokenType() != TokenType::COMMA);
+        if((*nextToken)->getTokenType() == TokenType::COMMA) getNextToken();
+
+    } while ((*(nextToken-1))->getTokenType() == TokenType::COMMA);
     fileout << "<FuncRParams>" << std::endl;
 }
 
 void Parsing::RelExp() {
     do {
         AddExp();
-        getNextToken();
-    } while ((*nextToken)->getTokenType() != TokenType::LSS || (*nextToken)->getTokenType() != TokenType::LEQ ||
-             (*nextToken)->getTokenType() != TokenType::GRE || (*nextToken)->getTokenType() != TokenType::GEQ);
-    fileout << "<RelExp>" << std::endl;
+        fileout << "<RelExp>" << std::endl;
+        if ((*nextToken)->getTokenType() == TokenType::LSS || (*nextToken)->getTokenType() == TokenType::LEQ ||
+            (*nextToken)->getTokenType() == TokenType::GRE || (*nextToken)->getTokenType() == TokenType::GEQ){
+            getNextToken();
+        }
+    } while ((*(nextToken-1))->getTokenType() == TokenType::LSS || (*(nextToken-1))->getTokenType() == TokenType::LEQ ||
+             (*(nextToken-1))->getTokenType() == TokenType::GRE || (*(nextToken-1))->getTokenType() == TokenType::GEQ);
+
 }
 
 void Parsing::EqExp() {
     do {
         RelExp();
-        getNextToken();
-    } while ((*nextToken)->getTokenType() != TokenType::EQL || (*nextToken)->getTokenType() != TokenType::NEQ);
-    fileout << "<EqExp>" << std::endl;
+        fileout << "<EqExp>" << std::endl;
+        if ((*(nextToken))->getTokenType() == TokenType::EQL || (*(nextToken))->getTokenType() == TokenType::NEQ) getNextToken();
+
+    } while ((*(nextToken-1))->getTokenType() == TokenType::EQL || (*(nextToken-1))->getTokenType() == TokenType::NEQ);
+
 }
 
 void Parsing::LAndExp() {
     do {
         EqExp();
-        getNextToken();
-    } while ((*nextToken)->getTokenType() != TokenType::AND);
-    fileout << "<LAndExp>" << std::endl;
+        fileout << "<LAndExp>" << std::endl;
+        if ((*nextToken)->getTokenType() == TokenType::AND) getNextToken();
+
+    } while ((*(nextToken-1))->getTokenType() == TokenType::AND);
+
 }
 
 void Parsing::Cond() {
@@ -269,21 +277,23 @@ void Parsing::Cond() {
 void Parsing::LOrExp() {
     do {
         LAndExp();
-        getNextToken();
-    } while ((*nextToken)->getTokenType() != TokenType::OR);
-    fileout << "<LOrExp>" << std::endl;
+        fileout << "<LOrExp>" << std::endl;
+        if((*nextToken)->getTokenType() == TokenType::OR) getNextToken();
+    } while ((*(nextToken-1))->getTokenType() == TokenType::OR);
+
 }
 
 void Parsing::VarDecl() {
-    //getNextToken();
+    getNextToken();
     if ((*nextToken)->getTokenType() != TokenType::INTTK) error();
 
     do {
-        getNextToken();
         VarDef();
+        if((*(nextToken))->getTokenType() == TokenType::COMMA || ((*(nextToken))->getTokenType() == TokenType::SEMICN)){
+            getNextToken();
+        }
         //std::cout << (*nextToken)->getTokenType() << std::endl;
-    } while ((*(nextToken))->getTokenType() == TokenType::COMMA);
-    getNextToken();
+    } while ((*(nextToken-1))->getTokenType() == TokenType::COMMA);
     fileout << "<VarDecl>" << std::endl;
 }
 
@@ -315,10 +325,11 @@ void Parsing::InitVal() {
             do {
                 // getNextToken();
                 InitVal();
-                getNextToken();
-            } while ((*nextToken)->getTokenType() == TokenType::COMMA);
+                if ((*(nextToken))->getTokenType() == TokenType::COMMA) getNextToken();
+            } while ((*(nextToken-1))->getTokenType() == TokenType::COMMA);
         }
         if ((*nextToken)->getTokenType() != TokenType::RBRACE) error();
+        else getNextToken(); //print }
     } else {
         Exp();
     }
@@ -327,9 +338,9 @@ void Parsing::InitVal() {
 
 void Parsing::FuncType() {
     //getNextToken();
-    std::cout << (*nextToken)->getTokenType() << std::endl;
+    //std::cout << (*nextToken)->getTokenType() << std::endl;
     if ((*nextToken)->getTokenType() == TokenType::VOIDTK || (*nextToken)->getTokenType() == TokenType::INTTK) {
-
+        getNextToken();
     } else {
         error();
     }
@@ -337,7 +348,7 @@ void Parsing::FuncType() {
 }
 
 void Parsing::FuncFParam() {
-    getNextToken();
+    //getNextToken();
     if ((*nextToken)->getTokenType() != TokenType::INTTK) error();
     getNextToken();
     if ((*nextToken)->getTokenType() != TokenType::IDENFR) error();
@@ -346,11 +357,11 @@ void Parsing::FuncFParam() {
         getNextToken();
         if ((*nextToken)->getTokenType() != TokenType::RBRACK) error();
         getNextToken();
-        while ((*nextToken)->getTokenType() != TokenType::LBRACK) {
+        while ((*nextToken)->getTokenType() == TokenType::LBRACK) {
+            getNextToken(); // print [
             ConstExp();
-            getNextToken();
             if ((*nextToken)->getTokenType() != TokenType::RBRACK) error();
-            getNextToken();
+            getNextToken(); // print ]
         }
     }
     fileout << "<FuncFParam>" << std::endl;
@@ -358,26 +369,29 @@ void Parsing::FuncFParam() {
 
 void Parsing::FuncFParams() {
     do {
-        getNextToken();
+        //getNextToken();
         FuncFParam();
-    } while ((*nextToken)->getTokenType() == TokenType::COMMA);
+        if((*nextToken)->getTokenType() == TokenType::COMMA) getNextToken();
+    } while ((*(nextToken-1))->getTokenType() == TokenType::COMMA);
+
     fileout << "<FuncFParams>" << std::endl;
 }
 
 void Parsing::FuncDef() {
-    FuncType();
-    getNextToken();
-    //std::cout << (*nextToken)->getTokenType() << std::endl;
-    if ((*nextToken)->getTokenType() != TokenType::IDENFR) error();
-    getNextToken();
-    if ((*nextToken)->getTokenType() != TokenType::LPARENT) error();
-    getNextToken();
-    if ((*nextToken)->getTokenType() == TokenType::INTTK) {
-        FuncFParams();
+    while( (*nextToken)->getTokenType() == TokenType::VOIDTK ||
+           (*nextToken)->getTokenType() == TokenType::INTTK && (*(nextToken + 1))->getTokenType() == TokenType::IDENFR &&
+           (*(nextToken + 2))->getTokenType() == TokenType::LPARENT){
+        FuncType();
+        getNextToken(); // print ident
+        getNextToken(); // print (
+        if((*(nextToken))->getTokenType() != TokenType::RPARENT){    // 有形参
+            FuncFParams();
+        }
+        getNextToken(); // print )    指向下一个符号
+        Block();
+        fileout  << "<FuncDef>" << std::endl;
     }
-    if ((*nextToken)->getTokenType() != TokenType::RPARENT) error();
-    Block();
-    fileout << "<FuncDef>" << std::endl;
+
 }
 
 void Parsing::MainFuncDef() {
@@ -428,19 +442,22 @@ void Parsing::Stmt() {
             if ((*nextToken)->getTokenType() != TokenType::SEMICN) error();
         }
     } else if ((*nextToken)->getTokenType() == TokenType::IFTK) {
-        getNextToken();
+        getNextToken(); // print if
         if ((*nextToken)->getTokenType() != TokenType::LPARENT) error();
+        else getNextToken(); // print (
         Cond();
-        getNextToken();
         if ((*nextToken)->getTokenType() != TokenType::RPARENT) error();
+        else getNextToken(); // print )
         Stmt();
-        getNextToken();
+       // getNextToken();
         if ((*nextToken)->getTokenType() == TokenType::ELSETK) {
+            getNextToken(); // print else
             Stmt();
         }
     } else if ((*nextToken)->getTokenType() == TokenType::WHILETK) {
         getNextToken();
         if ((*nextToken)->getTokenType() != TokenType::LPARENT) error();
+        else getNextToken(); // print (
         Cond();
         getNextToken();
         if ((*nextToken)->getTokenType() != TokenType::RPARENT) error();
